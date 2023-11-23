d1754e87a9fdf4ce385e0a536b3377f8da3164b4
HoangNguyet <nguyethoanghan@gmail.com>
2023-11-23 21:56:15 +0700

    FCFS

---
+++ FCFS/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace FCFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtArrivalTime = new TextBox();
+            txtBurstTime = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            lbProcesses = new ListBox();
+            button2 = new Button();
+            lbResult = new ListBox();
+            label3 = new Label();
+            tbxwait = new TextBox();
+            SuspendLayout();
+            // 
+            // txtArrivalTime
+            // 
+            txtArrivalTime.Location = new Point(128, 90);
+            txtArrivalTime.Margin = new Padding(4, 3, 4, 3);
+            txtArrivalTime.Name = "txtArrivalTime";
+            txtArrivalTime.Size = new Size(235, 30);
+            txtArrivalTime.TabIndex = 0;
+            // 
+            // txtBurstTime
+            // 
+            txtBurstTime.Location = new Point(462, 90);
+            txtBurstTime.Margin = new Padding(4, 3, 4, 3);
+            txtBurstTime.Name = "txtBurstTime";
+            txtBurstTime.Size = new Size(235, 30);
+            txtBurstTime.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(130, 44);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Arrival TIme";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(470, 45);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 23);
+            label2.TabIndex = 3;
+            label2.Text = "Burst Time";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(802, 89);
+            button1.Margin = new Padding(4, 3, 4, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(118, 33);
+            button1.TabIndex = 4;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // lbProcesses
+            // 
+            lbProcesses.FormattingEnabled = true;
+            lbProcesses.ItemHeight = 23;
+            lbProcesses.Location = new Point(135, 150);
+            lbProcesses.Margin = new Padding(4, 3, 4, 3);
+            lbProcesses.Name = "lbProcesses";
+            lbProcesses.Size = new Size(784, 119);
+            lbProcesses.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(724, 299);
+            button2.Margin = new Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(118, 33);
+            button2.TabIndex = 6;
+            button2.Text = "RunFCFS";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += btnRunFCFS;
+            // 
+            // lbResult
+            // 
+            lbResult.FormattingEnabled = true;
+            lbResult.ItemHeight = 23;
+            lbResult.Location = new Point(135, 366);
+            lbResult.Margin = new Padding(4, 3, 4, 3);
+            lbResult.Name = "lbResult";
+            lbResult.Size = new Size(784, 119);
+            lbResult.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(520, 532);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(221, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Thời gian chờ trung bình: ";
+            // 
+            // tbxwait
+            // 
+            tbxwait.Location = new Point(764, 529);
+            tbxwait.Margin = new Padding(4, 3, 4, 3);
+            tbxwait.Name = "tbxwait";
+            tbxwait.ReadOnly = true;
+            tbxwait.Size = new Size(155, 30);
+            tbxwait.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1000, 592);
+            Controls.Add(tbxwait);
+            Controls.Add(lbResult);
+            Controls.Add(button2);
+            Controls.Add(lbProcesses);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(txtBurstTime);
+            Controls.Add(txtArrivalTime);
+            Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FCFS";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtArrivalTime;
+        private TextBox txtBurstTime;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ListBox lbProcesses;
+        private Button button2;
+        private ListBox lbResult;
+        private Label label3;
+        private TextBox tbxwait;
+    }
+}
---
+++ FCFS/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+namespace FCFS
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = new List<Process>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtArrivalTime.Text) && !string.IsNullOrEmpty(txtBurstTime.Text))
+            {
+                int arrivalTime = Convert.ToInt32(txtArrivalTime.Text);
+                int burstTime = Convert.ToInt32(txtBurstTime.Text);
+
+                Process process = new Process
+                {
+                    Name = $"P{processes.Count + 1}",
+                    ArrivalTime = arrivalTime,
+                    BurstTime = burstTime
+                };
+
+                // Thêm tiến trình mới vào danh sách
+                processes.Add(process);
+
+                // Hiển thị thông tin tiến trình trong ListBox
+                lbProcesses.Items.Add($"{process.Name}: Thời gian vào: {arrivalTime}, Thời gian sử dụng CPU: {burstTime}");
+                txtArrivalTime.Text = "";
+                txtBurstTime.Text = "";
+            }
+            else
+            {
+                // Hiển thị thông báo nếu TextBox không chứa dữ liệu
+                MessageBox.Show("Vui lòng nhập thời gian đến và thời gian thực hiện.");
+            }
+        }
+
+        private void btnRunFCFS(object sender, EventArgs e)
+        {
+            if (processes.Count > 0)
+            {
+                // Sắp xếp tiến trình theo thời gian đến
+                processes.Sort((p1, p2) => p1.ArrivalTime.CompareTo(p2.ArrivalTime));
+
+                int totalWaitTime = 0;
+                //int totalTurnaroundTime = 0;
+                int currentTime = 0; // Thời gian bắt đầu dùng CPU
+                lbResult.Text = "Kết quả lịch trình FCFS:\r\n";
+
+                foreach (var process in processes)
+                {
+                    if (currentTime < process.ArrivalTime)
+                    {
+                        currentTime = process.ArrivalTime;
+                    }
+                    int waitTime = currentTime - process.ArrivalTime;
+                    //int turnaroundTime = waitTime + process.BurstTime;
+
+                    totalWaitTime += waitTime;
+                    //totalTurnaroundTime += turnaroundTime;
+                    lbResult.Items.Add($"{process.Name}: Thời gian bắt đầu = {currentTime}, Thời gian chờ đợi = {waitTime}, Thời gian hoàn thành = {currentTime + process.BurstTime}\r\n");
+                    currentTime += process.BurstTime;
+                }
+                // Tính thời gian chờ đợi trung bình
+                double averageWaitTime = (double)totalWaitTime / processes.Count;
+
+                //Hiển thị thời gian chờ trung bình
+                tbxwait.Text = Convert.ToString(averageWaitTime);
+            }
+        }
+    }
+
+    public class Process
+    {
+        public string Name { get; set; }
+        public int ArrivalTime { get; set; }
+        public int BurstTime { get; set; }
+    }
+}
